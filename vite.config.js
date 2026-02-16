3fb5a66a4991b2078cd0986592e34eb17f7cd8e7
vengateshfree/Ranipet <vengateshfree@gmail.com>
2026-02-16 20:23:18 +0530

    ./

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import sitemap from 'vite-plugin-sitemap'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react(), 
+     tailwindcss(),
+       sitemap({
+      hostname: 'https://ravanan-three.vercel.app/',
+      routes: [
+        '/',
+        '/about',
+        '/services',
+        '/contact',
+        '/bike-transport',
+        '/car-transport',
+        '/home-shifting',
+        '/office-shifting',
+      ]
+    })
+    
+    ],
 })
   
